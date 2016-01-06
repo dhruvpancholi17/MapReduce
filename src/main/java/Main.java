db78e37ec5a9f43ac7bb616946ec299f4787d740
Dhruv Pancholi <dhruv.pancholi@flipkart.com>
2016-01-06 11:51:56 +0530

    Changed Git Ignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,31 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /**
  * Created by dhruv.pancholi on 04/01/16.
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        FileInputStream in = null;
+        try {
+            Scanner scanner = new Scanner(new File("/Users/dhruv.pancholi/flipkart/input.txt"));
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String lastToken = null;
 
-        for(int i =0; i< args.length; i++){
-            System.out.println("Argument:"+i+" "+args[i]);
+                StringTokenizer s = new StringTokenizer(line, " ");
+                String year = s.nextToken();
+
+                while (s.hasMoreTokens()) {
+                    lastToken = s.nextToken();
+                }
+                int maxAvg = Integer.parseInt(lastToken);
+                System.out.println(maxAvg);
+            }
+        } catch (Exception e) {
+
         }
     }
 }
